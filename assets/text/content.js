01805ab1e8e4d207120a1b5a11ebbf2be76bf27d
Justin <whoamg@gmail.com>
2020-07-03 21:03:57 -0400

    no message

---
+++ assets/text/content.js
@@ -0,0 +1,6 @@
+var content = [
+	"command-options-answer" : ["Answer", "こたえる"],
+	"command-options-magic" : ["Magic", "まほう"],
+	"command-options-sense" : ["Sense", "みやぶる"],
+	"command-options-item" : ["Item", "アイテム"],
+]
