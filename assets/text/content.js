105c20efbe221b62c94d7bf81dc356fee5e6887c
Justin <whoamg@gmail.com>
2020-07-10 22:01:48 -0400

    no message

--- assets/text/content.js
+++ assets/text/content.js
@@ -3,6 +3,8 @@ var content = {};
 content.ENGLISH = 0;
 content.JAPANESE = 1;
 
+content["status-player-name"] = ["Cloud", "クラウド"];
+
 content["command-options-answer"] = ["Answer", "こたえる"];
 content["command-options-magic"] = ["Magic", "まほう"];
 content["command-options-sense"] = ["Sense", "みやぶる"];
