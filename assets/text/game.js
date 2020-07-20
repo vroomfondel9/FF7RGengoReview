01186a0c68bba440b18361b2135306a8a53b0c0c
Justin <whoamg@gmail.com>
2020-07-20 00:25:50 -0400

    no message

--- assets/text/game.js
+++ assets/text/game.js
@@ -668,7 +668,7 @@ function preprocessEnemyText(text) {
 	var lineBroken = [];
 	var spaceDelimited = obscuredText.split(spaceDelimiter);
 	var totalChars = 0;
-	for (var i = spaceDelimited.length - 1; i >= 0; i++) {
+	for (var i = spaceDelimited.length - 1; i >= 0; i--) {
 		console.log(i + ": " + spaceDelimited[i]);
 		lineBroken.push(spaceDelimited[i]);
 		totalChars = totalChars + spaceDelimited[i].length;
