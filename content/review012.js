121bd40f45624a1ec868fcea9dd76d668b60797d
Justin <whoamg@gmail.com>
2020-07-19 20:14:28 -0400

    no message

--- content/review012.js
+++ content/review012.js
@@ -1,9 +1,72 @@
 var enemies = [
 	{
-		"sentence": "",
-		"answers-kanji": [],
-		"answers-kana": [],
-		"answers-romaji": [],
+		"sentence": "セット＊完了＊",
+		"answers-kanji": ["完了"],
+		"answers-kana": ["かんりょう"],
+		"answers-romaji": ["kanryou"],
+		"answers-multiple": []
+	},
+	{
+		"sentence": "＊緊急事態＊発生　こちらは神羅カンパニー危機対策本部です",
+		"answers-kanji": ["緊急事態"],
+		"answers-kana": ["きんきゅうじたい"],
+		"answers-romaji": ["kinkyuujitai", "kinkyuu jitai"],
+		"answers-multiple": []
+	},
+	{
+		"sentence": "壱番魔光炉が　何者かの＊攻撃＊を受け　爆破されました",
+		"answers-kanji": ["攻撃"],
+		"answers-kana": ["こうげき"],
+		"answers-romaji": ["kougeki"],
+		"answers-multiple": []
+	},
+	{
+		"sentence": "壱番街　＊および＊八番街は　警戒区域に指定されました",
+		"answers-kanji": ["および"],
+		"answers-kana": ["および"],
+		"answers-romaji": ["oyobi"],
+		"answers-multiple": []
+	},
+	{
+		"sentence": "＊予想＊。。。以上",
+		"answers-kanji": ["予想"],
+		"answers-kana": ["よそう"],
+		"answers-romaji": ["yosou"],
+		"answers-multiple": []
+	},
+	{
+		"sentence": "星の命を＊救う＊ためにな",
+		"answers-kanji": ["救う"],
+		"answers-kana": ["すくう"],
+		"answers-romaji": ["sukuu"],
+		"answers-multiple": []
+	},
+	{
+		"sentence": "だよな　＊覚悟＊を決めて　始めたことだ",
+		"answers-kanji": ["覚悟"],
+		"answers-kana": ["かくご"],
+		"answers-romaji": ["kakugo"],
+		"answers-multiple": []
+	},
+	{
+		"sentence": "俺たちの活動は　星を救う　そのために　必要な＊犠牲＊は払う",
+		"answers-kanji": ["犠牲"],
+		"answers-kana": ["ぎせい"],
+		"answers-romaji": ["gisei"],
+		"answers-multiple": []
+	},
+	{
+		"sentence": "ぜーんぶ　俺が＊背負って＊やる",
+		"answers-kanji": ["背負って", "背負う"],
+		"answers-kana": ["せおって", "せおう"],
+		"answers-romaji": ["seotte", "se otte"],
+		"answers-multiple": []
+	},
+	{
+		"sentence": "それから　＊報酬＊も",
+		"answers-kanji": ["報酬"],
+		"answers-kana": ["ほしゅう"],
+		"answers-romaji": ["hoshuu"],
 		"answers-multiple": []
 	},
 ];
