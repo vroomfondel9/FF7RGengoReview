b6ebb387b8369a5ea2dd78994509675e624b6187
Justin <whoamg@gmail.com>
2020-07-20 20:43:28 -0400

    no message

--- assets/text/game.js
+++ assets/text/game.js
@@ -645,9 +645,10 @@ function createEnemies(game) {
 	enemy.animating = false;
 	enemy.index = -1;
 	
-	enemy.damage = game.add.text(enemy.x, enemy.y, 'Miss', {fontSize: '26px', fill: fontColor,  wordWrap: {width: wrapWidth, useAdvancedWrap: true}});
+	enemy.damage = game.add.text(enemy.x, enemy.y, 'Miss', {fontSize: '32px', fill: '#FFF'});
 	enemy.damage.setStroke('#000', 2);
 	enemy.damage.setOrigin(0.5);
+	console.log('omg1');
 	
 	var floatHeight = env.ground.foreground.getBounds().y - (env.ground.foreground.getBounds().height / 2) - 25;
 	var textbottom = enemy.getBounds().y + (numLines - 1) * lineHeight;
