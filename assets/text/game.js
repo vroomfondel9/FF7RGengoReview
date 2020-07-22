53904dff739afd20c027798773ac4240dce97911
Justin <whoamg@gmail.com>
2020-07-21 23:14:33 -0400

    no message

--- assets/text/game.js
+++ assets/text/game.js
@@ -110,7 +110,7 @@ function update (time, delta)
 		//ui.status.time.resume();
 		console.log("time: " + time);
 		console.log("delta: " + delta);
-		console.log(scene.scene.get("default"));
+		console.log(this);
 		//game.pause("default");
 	}
 }
