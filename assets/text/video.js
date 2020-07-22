d665f08eb59cf108cd60b84c0a2eea83ab0595fa
Justin <whoamg@gmail.com>
2020-07-21 21:07:30 -0400

    no message

--- assets/text/video.js
+++ assets/text/video.js
@@ -6,37 +6,38 @@ iframe_api_tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(iframe_api_tag, firstScriptTag);
 
-// 3. This function creates an <iframe> (and YouTube player)
-//    after the API code downloads.
+// Called when Async Youtube Iframes API load complete
 function onYouTubeIframeAPIReady() {
 	videoPlayer = new YT.Player('video-container', {
-	  events: {
-		'onReady': onPlayerReady,
-		'onStateChange': onPlayerStateChange
-	  }
+		events: {
+			'onReady': onPlayerReady,
+			'onStateChange': onPlayerStateChange
+		}
 	});
 }
 
-// 4. The API will call this function when the video player is ready.
+// Called when video ready to play
 function onPlayerReady(event) {
 	console.log('on ready called');
 	event.target.mute();
 	event.target.playVideo();
 }
 
-// 5. The API calls this function when the player's state changes.
-//    The function indicates that when playing a video (state=1),
-//    the player should play for six seconds and then stop.
-var done = false;
+// Status key:
+// -1: Unstarted	(no enum)
+//  0: Ended		YT.PlayerState.ENDED
+//  1: Playing		YT.PlayerState.PLAYING
+//  2: Paused		YT.PlayerState.PAUSED
+//  3: Buffering	YT.PlayerState.BUFFERING
+//  5: Cued			YT.PlayerState.CUED
 function onPlayerStateChange(event) {
 	console.log('on state change called: ' + event.data);
-if (event.data == YT.PlayerState.PLAYING && !done) {
-  setTimeout(unmuteVideo, 6000);
-  done = true;
-}
+	if (event.data == YT.PlayerState.PLAYING) {
+		setTimeout(unmuteVideo, 6000);
+	}
 }
 function unmuteVideo() {
-	videoPlayer.unmute();
+	videoPlayer.unMute();
 }
 
 function stopVideo() {
