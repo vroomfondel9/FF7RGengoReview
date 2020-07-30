134b0dd57f05494af29f3ac6fb3060886c3eb41a
Justin <whoamg@gmail.com>
2020-07-29 21:07:38 -0400

    no message

--- assets/text/common.js
+++ assets/text/common.js
@@ -29,3 +29,20 @@ var content = {
 		}
 	}
 };
+
+preprocessVideoDetails();
+
+function preprocessVideoDetails() {
+	//Should probably do some validation but that's boring!
+	populateEndTimestamps();
+}
+
+function populateEndTimestamps() {
+	videoDetails.intro.end = videoDetails.questions[0].intro;
+	videoDetails.questions[videoDetails.questions.length - 1].end = videoDetails.outro.fanfare;
+	
+	for (var i = 0; i <= videoDetails.questions.length - 2; i++) {
+		videoDetails.questions[i].end = videoDetails.questions[i + 1].intro;
+		console.log(i + ': ' + videoDetails.questions[i].end);
+	}
+}
